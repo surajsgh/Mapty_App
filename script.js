5ccc83571d4d0ba141c581d0890e6f0a567cd6cf
Suraj Singh <surajsingh220698@outlook.com>
2021-04-06 21:46:17 +0530

    Display multiple map marker on click.

--- script.js
+++ script.js
@@ -28,10 +28,27 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coordinates)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (e) {
+        // console.log(e);
+        // const { latlng } = e.latlng;
+        // console.log(e.latlng);
+        // console.log(lat, lng);
+        const { lat, lng } = e.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout!')
+          .openPopup();
+      });
     },
     function () {
       alert(`Couldn't get your location.`);
