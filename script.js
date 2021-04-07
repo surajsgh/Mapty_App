2c160d24597844bba92fb6283521b0a94c276ed3
Suraj Singh <surajsingh220698@outlook.com>
2021-04-07 11:44:08 +0530

    Create the parent-child classes as per the given architecture paradigm.

--- script.js
+++ script.js
@@ -11,8 +11,48 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+// Parent Class
+class Workout {
+  date = new Date();
+  id = (new Date() + '').slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords; //  [lat, lng]
+    this.distance = distance; // In km
+    this.duration = duration; // In min
+  }
+}
+
+// Child Class
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return calcPace();
+  }
+}
+
+// Child Class
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.speed();
+  }
+
+  speed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// App Class
+// Application Architecture
 class App {
   #map;
   #mapEvent;
